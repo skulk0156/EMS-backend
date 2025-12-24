eccb703c9f8bedfa47662ebda2250bf8b8bffcfb
Shubham Kulkarni <skulk156.pc@gmail.com>
2025-12-24 12:15:30 +0530

    server_port_config_by_sk

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use('/api/projects', projectRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
+  .then(() => console.log("Your Dababase is Connected to your Server ✅"))
   .catch((err) => console.error("❌ MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:${PORT}`));
